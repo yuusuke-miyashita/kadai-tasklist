abc48a370b1a9031d147dbb706589c6472000901
yuusuke.miyashita <yuusuke.6125@outlook.jp>
2020-03-03 13:09:56 +0000

    Procfile

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+     
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
+            
+            return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+            
+           
+        }
+        return view('welcome');
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       $task = new Task;
+
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+      
+        $task = new Task;
+        $task->user_id = \Auth::id();
+        $task->status = $request->status; 
+        $task->content = $request->content;
+        $task->save();
+
+        return redirect('/'); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+         $task = Task::find($id);
+         if (\Auth::id() === $task->user_id) {
+
+        return view('tasks.show', [
+            'task' => $task,
+        ]);}
+         return redirect('/');
+        
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+         $task = Task::find($id);
+
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+       
+        return redirect('/');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+       $this->validate($request, [
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+       
+       
+        $task = Task::find($id);
+        if (\Auth::id() === $task->user_id) {
+            $task->status = $request->status;  
+            $task->content = $request->content;
+            $task->user_id = \Auth::id();
+            $task->save();
+        }
+
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        if (\Auth::id() === $task->user_id) {
+            $task->delete();
+        }
+        
+        
+
+       return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'TasksController@index');
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::resource('tasks', 'TasksController');
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+
+Route::group(['middleware' => ['auth']], function () {
+    
+    Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy']]);
 });
