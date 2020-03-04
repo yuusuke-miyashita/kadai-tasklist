745833ab707fd3e99682a2a767f6902ce30fd34c
yuusuke.miyashita@m08035266125@yahoo.co.jp <yuusuke.6125@outlook.jp>
2020-03-04 07:06:46 +0000

    init

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -93,10 +93,11 @@ class TasksController extends Controller
     public function edit($id)
     {
          $task = Task::find($id);
+          if (\Auth::id() === $task->user_id) {
 
         return view('tasks.edit', [
             'task' => $task,
-        ]);
+        ]);}
        
         return redirect('/');
     }
